30dc8696c48c6cffead62f14d9475d3e7603d74a
weilonglin <randomfriend@live.nl>
2020-09-17 15:20:32 +0200

    heroku

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,26 +1,26 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const LoadingDiv = styled.div`
-	width: 100vw;
-	height: 100vh;
-	margin: 0 auto;
-	display: flex;
-	align-items: stretch;
-	flex-direction: column;
-	font-size: 20px;
+  width: 100vw;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  align-items: stretch;
+  flex-direction: column;
+  font-size: 20px;
 
-	p {
-		align-content: center;
-		align-items: stretch;
-		margin: 0 auto;
-	}
-`
+  p {
+    align-content: center;
+    align-items: stretch;
+    margin: 0 auto;
+  }
+`;
 
 export const Loading = () => {
-	return (
-		<LoadingDiv>
-			<p>Loading....</p>
-		</LoadingDiv>
-	)
-}
+  return (
+    <LoadingDiv>
+      <p>Loading...</p>
+    </LoadingDiv>
+  );
+};
